77345d2f362834b55d244247b239e27493a31662
L3NNY <35822194+L3NNY0969@users.noreply.github.com>
2018-06-17 23:35:47 -0500

    lmao you still have this

--- bot.py
+++ bot.py
@@ -88,7 +88,7 @@ async def upvote(ctx):
 @bot.command()
 async def expose(ctx):
     """someone got expose"""
-    await ctx.send ("Shut up {user.mentiom}! Stop lying! I **know** that you have been cheating on Venessia and dating Charlie's girlfriend! ~~even saw you and Charlie's girlfriend h00king uP in a BARN~~ So, {user.mention}, STOP LYING TO CHARLIE OR HE WILL SUE YOU FOR SEXUAL ALLIGATIONS FOR DATING HIS GIRL! ~~not even sure if yu can do that tho~~") 
+    await ctx.send ("Shut up {user.mention}! I **know** that you have been cheating on Venessia and dating Charlie's girlfriend! ~~even saw you and Charlie's girlfriend h00king uP in a BARN~~ So, {user.mention}, STOP LYING TO CHARLIE OR HE WILL SUE YOU FOR SEXUAL ALLIGATIONS FOR DATING HIS GIRL! ~~not even sure if yu can do that tho~~") 
 	
 
 @bot.command()
